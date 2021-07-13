babc4a6a2a0de32fe9154ef0de442b71eedae23b
Cole Purtzer <65046717+Cole-DeltaSpaceSys@users.noreply.github.com>
2021-07-13 02:08:22 -0700

    Update orientation.h

--- src/orientation.h
+++ src/orientation.h
@@ -7,15 +7,19 @@ class Orientation {
   public:
     void Update(double gyroRawX, double gyroRawY, double gyroRawZ, double dtseconds);
     void toEuler();
+    void toQuaternion(double yaw, double pitch, double roll);
     void toRAD();
 
     double Ax, Ay, Az;   
-    double localGyroRawX, localGyroRawY, localGyroRawZ;
-    double Qw[4];
-    double Ax_RAD, Ay_RAD, Az_RAD;
     double AxRAD, AyRAD, AzRAD;
+    double q[4] = {1, 0, 0, 0};
+
+    private:
+    double Qw[4] = {1, 0, 0, 0};
     double Quat_ori[4] = {1, 0, 0, 0};
-    double Quat_dot[4];   
+    double Quat_dot[4] = {1, 0, 0, 0}; 
+    double localGyroRawX, localGyroRawY, localGyroRawZ;
+    double Ax_RAD, Ay_RAD, Az_RAD;
 };
 
 #endif 
